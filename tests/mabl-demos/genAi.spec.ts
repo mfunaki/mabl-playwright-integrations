b52e01ee5699e8f034a40f5cde0ecc74490ab940
Masahiko Funaki <mfunaki@gmail.com>
2025-06-22 08:08:54 +0900

    日本語サイトの検証用にGenAIアサーションのテストを更新

--- tests/mabl-demos/genAi.spec.ts
+++ tests/mabl-demos/genAi.spec.ts
@@ -4,10 +4,10 @@ test('Evaluate a GenAI assertion to validate the state of a page', async ({
   page,
   mabl,
 }) => {
-  await page.goto('https://www.mabl.com');
+  await page.goto('https://www.mabl.com/ja/');
 
   const prompt =
-    'Please validate that the web page you are looking at is a marketing page for a company named mabl which does automated UI testing';
+    '表示されているWebページがUIテストを自動化するmablという企業の日本語サイトかどうかを検証し、検証結果を日本語で通知。';
   const result = await mabl.evaluateGenAiAssertion(page, prompt);
   console.log('GEN AI EXPLANATION');
   console.log(result.explanation);
